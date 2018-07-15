b337f133e2d28cd7c0a908011fd1a1ac89994ad0
Like Zheng <keithleaves@gmail.com>
2018-07-15 22:43:06 +0800

    day1

---
+++ day1.js
@@ -0,0 +1,20 @@
+// This problem was recently asked by Google.
+//
+// Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+//
+// For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+//
+// Bonus: Can you do this in one pass?
+
+// Method 1
+function foo(array, k) {
+  return array.some((element, index) => {
+    for (let i = index + 1; i < array.length; i++) {
+      if (element + array[i] == k) {
+        return true;
+      }
+    }
+  });
+}
+
+console.log(foo([10, 15, 3, 7], 16));
